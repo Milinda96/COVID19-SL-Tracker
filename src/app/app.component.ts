c760d5e0acc4a4fa83871b47e6a79e4efba15f1b
Milinda Senaka <milindasenaka.1@gmail.com>
2020-12-14 01:55:30 +0530

    remove homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "./http.service";
+import * as particleConfig from "./../assets/data/particles.json";
+import { Container } from "tsparticles/dist/Core/Container";
 
 import { Platform } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
@@ -9,11 +12,20 @@ import { StatusBar } from "@ionic-native/status-bar/ngx";
   templateUrl: "app.component.html",
   styleUrls: ["app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  mediData: Object;
+  state = {
+    local_active_cases: null,
+    update_date_time: null,
+    local_deaths: null,
+    local_new_cases: null,
+    local_recovered: null,
+  };
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private http: HttpService
   ) {
     this.initializeApp();
   }
@@ -23,5 +35,46 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  ngOnInit(): void {
+    this.http.getMediData().subscribe((data) => {
+      this.mediData = data;
+      for (const i in this.mediData) {
+        if (Object.prototype.hasOwnProperty.call(this.mediData, i)) {
+          const element = this.mediData[i];
+          this.state = element;
+        }
+      }
+      console.log(this.state);
+    });
+  }
+  id = "tsparticles";
+  particlesOptions = particleConfig["default"];
+
+  particlesLoaded(container: Container): void {
+    setTimeout(async () => {
+      const canvas = container.canvas.element;
+      if (!canvas) {
+        console.log("no canvas");
+        return;
+      }
+
+      const pxRatio = container.retina.pixelRatio;
+      container.canvas.size.width = canvas.offsetWidth * pxRatio;
+      container.canvas.size.height = canvas.offsetHeight * pxRatio;
+      canvas.width = container.canvas.size.width;
+      canvas.height = container.canvas.size.height;
+
+      console.log(canvas);
+      /* density particles enabled */
+      container.densityAutoParticles();
+      for (const [, plugin] of container.plugins) {
+        if (plugin.resize !== undefined) {
+          plugin.resize();
+        }
+      }
+
+      await container.refresh();
+    }, 50);
   }
 }
